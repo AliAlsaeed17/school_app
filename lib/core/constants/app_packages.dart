8fd9af5a115960b7debb7fa0aeef2954c29a36d3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-22 02:41:19 -0700

    Create app packages class

---
+++ lib/core/constants/app_packages.dart
@@ -0,0 +1,13 @@
+export 'package:flutter/material.dart';
+export 'package:school_app/core/constants/app_images.dart';
+export 'package:flutter_screenutil/flutter_screenutil.dart';
+export 'package:school_app/core/constants/app_theme.dart';
+export 'package:school_app/core/constants/ui_text_style.dart';
+export 'package:get/get.dart';
+export 'dart:async';
+export 'package:school_app/core/constants/app_routes.dart';
+export 'package:school_app/view/splash/screens/splash_screen.dart';
+export 'package:school_app/core/constants/app_colors.dart';
+export 'package:school_app/view/splash/widgets/splash_logo_app_widget.dart';
+export 'package:school_app/view/splash/widgets/splash_name_app_widget.dart';
+export 'package:school_app/logic/controllers/auth_controller.dart';
