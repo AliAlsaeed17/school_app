fdb80632d1724024c4d7851d2a58db2809017f22
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-24 00:08:55 -0700

    Make screen as statefull widget and refactor screen code

--- lib/view/auth/login/screens/login_screen.dart
+++ lib/view/auth/login/screens/login_screen.dart
@@ -1,145 +1,105 @@
 import 'package:school_app/core/constants/app_packages.dart';
 
-class LoginScreen extends StatelessWidget {
-  LoginScreen({super.key});
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
 
-  final AuthController authController = Get.put(AuthController());
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  late TextEditingController phoneController;
+  late TextEditingController passwordController;
+  final _keyForm = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    phoneController = TextEditingController();
+    passwordController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    phoneController.clear();
+    phoneController.dispose();
+    passwordController.clear();
+    passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
+    final authCubit = BlocProvider.of<AuthCubit>(context);
     return Directionality(
       textDirection: TextDirection.rtl,
       child: Scaffold(
         resizeToAvoidBottomInset: false,
-        backgroundColor: AppColors.primary,
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                margin: const EdgeInsets.only(top: 100),
-                alignment: Alignment.center,
-                child: Text(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 100),
+            child: Column(
+              children: [
+                Text(
                   "مرحباً بعودتك!",
                   style: AppTheme.themeArabic.textTheme.titleLarge,
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              const Center(
-                child: CircleAvatar(
-                  radius: 90,
-                  backgroundColor: AppColors.primary,
-                  backgroundImage: AssetImage(AppImages.schoolSchildren),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                style: UITextStyle.titleBold.copyWith(color: Colors.black),
-                controller: authController.phoneTextController,
-                decoration: InputDecoration(
-                  filled: true,
-                  fillColor: AppColors.white,
-                  border: const OutlineInputBorder(),
-                  hintText: 'أدخل رقم هاتفك',
-                  hintStyle: UITextStyle.normalSmall,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide:
-                        const BorderSide(width: 2, color: AppColors.lightBlack),
-                    borderRadius: BorderRadius.circular(20.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: const BorderSide(color: AppColors.gray)),
-                ),
-                maxLines: 1,
-              ),
-              const SizedBox(height: 20),
-              Obx(() {
-                return TextFormField(
-                  style: UITextStyle.titleBold.copyWith(color: Colors.black),
-                  controller: authController.passwordTextController,
-                  decoration: InputDecoration(
-                    filled: true,
-                    fillColor: AppColors.white,
-                    suffixIcon: IconButton(
-                      icon: Icon(!authController.passwordVisible.value
-                          ? Icons.visibility_off
-                          : Icons.visibility),
-                      color: AppColors.iconColor,
-                      onPressed: () {
-                        authController.togglePasswordVisible();
-                      },
+                const VerticalSizedBox(30),
+                Container(
+                  height: 180,
+                  decoration: const BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(AppImages.schoolSchildren),
                     ),
-                    border: const OutlineInputBorder(),
-                    hintText: 'أدخل كلمة المرور الخاصة بك',
-                    hintStyle: UITextStyle.normalSmall,
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(
-                          width: 2, color: AppColors.lightBlack),
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(20),
-                        borderSide: const BorderSide(color: AppColors.gray)),
                   ),
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: !authController.passwordVisible.value,
-                  maxLines: 1,
-                );
-              }),
-              const SizedBox(
-                height: 40,
-              ),
-              Obx(() {
-                return MaterialButton(
-                  height: 56,
-                  minWidth: Get.width,
-                  color: AppColors.studentTime,
-                  shape: const RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
+                ),
+                const VerticalSizedBox(20),
+                Form(
+                  key: _keyForm,
+                  child: Column(
                     children: [
-                      if (authController.sending.value)
-                        const CircularProgressIndicator(
-                          color: AppColors.white,
+                      AppTextFormField(
+                        controller: phoneController,
+                        hintText: 'أدخل رقم هاتفك',
+                      ),
+                      const VerticalSizedBox(20),
+                      AppTextFormField(
+                        controller: passwordController,
+                        hintText: 'أدخل كلمة المرور الخاصة بك',
+                        keyboardType: TextInputType.visiblePassword,
+                        obscureText: true,
+                        suffix: IconButton(
+                          icon: Icon('true' == true
+                              ? Icons.visibility_off
+                              : Icons.visibility),
+                          color: AppColors.iconColor,
+                          onPressed: () {},
                         ),
-                      const Text('تسجيل الدخول', style: UITextStyle.titleBold)
+                      ),
+                      const VerticalSizedBox(40),
+                      AppButton(
+                        text: 'تسجيل الدخول',
+                        onPressed: () {
+                          // if (_keyForm.currentState!.validate()) {
+                          //   authCubit.login(
+                          //     phoneController.text.trim(),
+                          //     passwordController.text.trim(),
+                          //   );
+                          // }
+                          Navigator.pushNamed(context, AppRoutes.homeScreen);
+                        },
+                      ),
+                      const VerticalSizedBox(10),
+                      AppButton(
+                        text: 'طلب تسجيل',
+                        onPressed: () {},
+                      )
                     ],
                   ),
-                  onPressed: () async {
-                    //await loginController.login();
-                  },
-                );
-              }),
-              const SizedBox(
-                height: 10,
-              ),
-              Flexible(
-                  child: MaterialButton(
-                height: 56,
-                minWidth: Get.width,
-                color: AppColors.studentTime,
-                shape: const RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: const [
-                    Text('طلب تسجيل', style: UITextStyle.titleBold)
-                  ],
-                ),
-                onPressed: () {
-                  //Get.toNamed(AppRoutes.registration);
-                },
-              ))
-            ],
+                )
+              ],
+            ),
           ),
         ),
       ),
