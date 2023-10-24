a272bbb604390658495e2a2ac8dd4abc2c7ca866
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-24 00:08:05 -0700

    Create app size class

---
+++ lib/core/constants/app_sizes.dart
@@ -0,0 +1,6 @@
+import 'package:school_app/core/constants/app_packages.dart';
+
+class AppSizes {
+  //Border Radius
+  static BorderRadius radius22 = const BorderRadius.all(Radius.circular(22));
+}
