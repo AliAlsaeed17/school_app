99c0296d5e511be30b4f5401b353278a1b7f81a8
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-24 00:09:48 -0700

    Create user client and repo class

---
+++ lib/data/data_scource/remote/user_client.dart
@@ -0,0 +1,26 @@
+import 'package:school_app/core/constants/app_packages.dart';
+
+class UserClient {
+  Dio dio = Dio();
+
+  Future<dynamic> login(phoneNumber, password) async {
+    var response = await dio.post(
+      AppApiLinks.baseUrl + AppApiLinks.loginApi,
+      options: Options(
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      ),
+      data: jsonEncode(<String, dynamic>{
+        "username": phoneNumber,
+        "password": password,
+      }),
+    );
+    print(response.data);
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      return null;
+    }
+  }
+}
---
+++ lib/data/repositories/user_repo.dart
@@ -0,0 +1,14 @@
+import 'package:school_app/core/constants/app_packages.dart';
+
+class UserRepo {
+  var client = UserClient();
+
+  Future<bool> login(phoneNumber, password) async {
+    var data = await client.login(phoneNumber, password);
+    print(data);
+    if (data != null) {
+      return true;
+    }
+    return false;
+  }
+}
