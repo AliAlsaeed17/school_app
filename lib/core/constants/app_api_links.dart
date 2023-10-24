ba52da5c42acda25e54938fd99e541311747d44c
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-24 00:06:22 -0700

    Create api link class

---
+++ lib/core/constants/app_api_links.dart
@@ -0,0 +1,6 @@
+class AppApiLinks {
+//-----------------------------Root-----------------------------------
+  static const baseUrl = 'http://school.brain.sy/api/v1/';
+//-----------------------------Auth-----------------------------------
+  static const loginApi = '${baseUrl}login';
+}
