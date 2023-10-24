b166689a11d1ee0670a6f7c78af6d27aa3c9db9a
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-24 00:08:24 -0700

    Add color scheme and input decoration theme

--- lib/core/constants/app_theme.dart
+++ lib/core/constants/app_theme.dart
@@ -5,6 +5,19 @@ class AppTheme {
     fontFamily: 'Hacen Tunisia Lt',
     splashColor: null,
     primarySwatch: null,
+    colorScheme: const ColorScheme(
+      brightness: Brightness.dark,
+      primary: AppColors.primary,
+      onPrimary: AppColors.primary,
+      secondary: AppColors.secondary,
+      onSecondary: AppColors.secondary,
+      error: AppColors.alert,
+      onError: AppColors.alert,
+      background: AppColors.primary,
+      onBackground: AppColors.primary,
+      surface: AppColors.white,
+      onSurface: AppColors.white,
+    ),
     textTheme: const TextTheme(
       titleLarge: TextStyle(
         fontWeight: FontWeight.bold,
@@ -26,7 +39,38 @@ class AppTheme {
       ),
       bodySmall: TextStyle(
         color: AppColors.white,
-        fontSize: 12,
+        fontSize: 16,
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: AppColors.white,
+      border: const OutlineInputBorder(),
+      hintStyle: const TextStyle(
+        color: AppColors.labelTextColor,
+        fontSize: 16,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: const BorderSide(width: 2, color: AppColors.lightBlack),
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: AppColors.gray)),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        elevation: const MaterialStatePropertyAll(0),
+        backgroundColor:
+            const MaterialStatePropertyAll<Color>(AppColors.secondary),
+        shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+          RoundedRectangleBorder(
+            borderRadius: AppSizes.radius22,
+          ),
+        ),
+        minimumSize: const MaterialStatePropertyAll<Size>(
+          Size(double.infinity, 55),
+        ),
       ),
     ),
   );
