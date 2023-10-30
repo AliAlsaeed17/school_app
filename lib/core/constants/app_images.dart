3809e4754a7f5972392a52823e38cc9edc941850
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-30 00:08:50 -0700

    Add new images

--- lib/core/constants/app_images.dart
+++ lib/core/constants/app_images.dart
@@ -2,4 +2,6 @@ class AppImages {
   static const root = 'assets/images/';
   static const logo = '${root}app_icon.jpg';
   static const schoolSchildren = "${root}schoolchildren.png";
+  static const schoolBuilding = "${root}school-ph.jpg";
+  static const studentsSchool = '${root}studentschool.png';
 }
