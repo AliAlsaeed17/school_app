d8e0d88f0944837785f1b2e04d79fc45605769c5
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-30 00:09:05 -0700

    Add new border radius

--- lib/core/constants/app_sizes.dart
+++ lib/core/constants/app_sizes.dart
@@ -2,5 +2,12 @@ import 'package:school_app/core/constants/app_packages.dart';
 
 class AppSizes {
   //Border Radius
-  static BorderRadius radius22 = const BorderRadius.all(Radius.circular(22));
+  static final BorderRadius radius10 = BorderRadius.circular(10);
+  static BorderRadius radius15 = BorderRadius.circular(15);
+  static BorderRadius radius22 = BorderRadius.circular(22);
+  static BorderRadius radius25 = BorderRadius.circular(25);
+  static BorderRadius radiusBottomRight45 =
+      const BorderRadius.only(bottomRight: Radius.circular(45));
+  static BorderRadius radiusTop45 = const BorderRadius.only(
+      topLeft: Radius.circular(45), topRight: Radius.circular(45));
 }
