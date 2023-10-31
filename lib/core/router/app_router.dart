74e3951a1cffeaacd4cda35284d08f32dbce8a74
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-31 03:22:51 -0700

    Add vacations screen to router and bloc provider to subjects screen navigation

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:school_app/core/constants/app_packages.dart';
+import 'package:school_app/logic/cubits/subjects/subjects_cubit.dart';
 import 'package:school_app/view/home/screens/home_screen.dart';
 import 'package:school_app/view/homeworks/screens/homeworks_screen.dart';
 import 'package:school_app/view/subjects/screens/subjects_screen.dart';
+import 'package:school_app/view/vacations/screens/vacations_screen.dart';
 
 class AppRouter {
   static Route? generateRoute(RouteSettings settings) {
@@ -23,11 +25,18 @@ class AppRouter {
         );
       case AppRoutes.subjectsScreen:
         return MaterialPageRoute(
-          builder: (_) => SubjectsScreen(),
+          builder: (_) => BlocProvider(
+            create: (context) => SubjectsCubit(),
+            child: SubjectsScreen(),
+          ),
         );
       case AppRoutes.homeworksScreen:
         return MaterialPageRoute(
           builder: (_) => HomeworksScreen(),
+        );
+      case AppRoutes.vacationsScreen:
+        return MaterialPageRoute(
+          builder: (_) => VacationsScreen(),
         );
     }
     return null;
