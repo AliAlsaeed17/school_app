44c866b9a3124fbd7d61c0033bc2f6307c9a79a6
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-31 03:16:51 -0700

    Create vacations screen

---
+++ lib/view/vacations/screens/vacations_screen.dart
@@ -0,0 +1,76 @@
+import 'package:school_app/core/constants/app_packages.dart';
+import 'package:school_app/view/vacations/widgets/vacation_item.dart';
+import 'package:school_app/view/vacations/widgets/vacation_shimmer.dart';
+
+class VacationsScreen extends StatefulWidget {
+  const VacationsScreen({super.key});
+
+  @override
+  State<VacationsScreen> createState() => _VacationsScreenState();
+}
+
+class _VacationsScreenState extends State<VacationsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Scaffold(
+        appBar: mainAppBar(),
+        drawer: const AppDrawer(),
+        body: Padding(
+          padding: const EdgeInsets.all(15),
+          child: 1 == 2
+              ? SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return const vacationShimmer();
+                    },
+                    separatorBuilder: (context, index) =>
+                        const VerticalSizedBox(10),
+                    itemCount: 10,
+                  ),
+                )
+              : RefreshIndicator(
+                  onRefresh: () async {
+                    //homeworksController.getHomeworks();
+                  },
+                  child: 'true' == true
+                      ? SizedBox(
+                          height: MediaQuery.of(context).size.height,
+                          child: Center(
+                            child: ListView(
+                              shrinkWrap: true,
+                              children: [
+                                Text(
+                                  "لايوجد عطل!",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .titleLarge!
+                                      .copyWith(
+                                        fontSize: 16,
+                                        color: AppColors.lightBlack,
+                                      ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      : SizedBox(
+                          child: ListView.separated(
+                            itemBuilder: (context, index) {
+                              return const VacationItem();
+                            },
+                            separatorBuilder: (context, index) =>
+                                const VerticalSizedBox(10),
+                            itemCount: 10,
+                          ),
+                        ),
+                ),
+        ),
+        bottomNavigationBar: const AppBottomNavigationBar(),
+      ),
+    );
+  }
+}
