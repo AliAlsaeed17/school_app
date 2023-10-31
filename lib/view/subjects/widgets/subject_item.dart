ef6c79d9f5b43a6c0395e68efbe8b2005ea845e1
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-31 03:21:31 -0700

    Add subjects parameter and update widget based on it

--- lib/view/subjects/widgets/subject_item.dart
+++ lib/view/subjects/widgets/subject_item.dart
@@ -1,37 +1,49 @@
 import 'package:school_app/core/constants/app_packages.dart';
 
 class SubjectItem extends StatelessWidget {
-  const SubjectItem({super.key});
+  const SubjectItem({super.key, required this.subject});
+
+  final Subject subject;
 
   @override
   Widget build(BuildContext context) {
     return InkWell(
       onTap: () {},
-      child: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: AppSizes.radius22,
-              color: AppColors.primary,
-              image: const DecorationImage(
-                image: AssetImage(
-                  AppImages.studentsSchool,
-                ),
-                fit: BoxFit.cover,
-                opacity: .1,
-              ),
-            ),
-          ),
-          Center(
-            child: Text(
-              'Subject Name',
-              style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                    color: AppColors.white,
-                    fontSize: 25,
+      child: ClipRRect(
+        borderRadius: AppSizes.radius22,
+        child: CachedNetworkImage(
+          imageUrl: subject.image,
+          placeholder: (context, url) =>
+              const Center(child: LoadingItem(color: AppColors.secondary)),
+          imageBuilder: (context, imageProvider) {
+            return Stack(
+              children: [
+                Opacity(
+                  opacity: .4,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: imageProvider,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
                   ),
-            ),
-          ),
-        ],
+                ),
+                Center(
+                  child: Text(
+                    subject.name,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: AppColors.white,
+                          fontSize: 25,
+                        ),
+                  ),
+                ),
+              ],
+            );
+          },
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
