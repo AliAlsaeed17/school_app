774e8837ce54e08c7d32dee339d80615b3ac1dc7
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-10-31 03:13:21 -0700

    Add new links

--- lib/core/constants/app_api_links.dart
+++ lib/core/constants/app_api_links.dart
@@ -1,6 +1,9 @@
 class AppApiLinks {
 //-----------------------------Root-----------------------------------
   static const baseUrl = 'http://school.brain.sy/api/v1/';
+  static const storageBaseUrl = 'http://school.brain.sy/storage/';
 //-----------------------------Auth-----------------------------------
   static const loginApi = '${baseUrl}login';
+//----------------------------Subjects--------------------------------
+  static const subjectWithHomework = "${baseUrl}subjectWithHomework/1/1";
 }
