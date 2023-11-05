0da005534fcaf5166870654816ffc401a6015e2f
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-05 05:00:15 -0800

    Update screen with homeworks cubit to display cubits

--- lib/view/homeworks/screens/homeworks_screen.dart
+++ lib/view/homeworks/screens/homeworks_screen.dart
@@ -1,4 +1,5 @@
 import 'package:school_app/core/constants/app_packages.dart';
+import 'package:school_app/logic/cubits/homeworks/homeworks_cubit.dart';
 
 class HomeworksScreen extends StatefulWidget {
   const HomeworksScreen({super.key});
@@ -17,55 +18,60 @@ class _HomeworksScreenState extends State<HomeworksScreen> {
         drawer: const AppDrawer(),
         body: Padding(
           padding: const EdgeInsets.all(15),
-          child: 1 == 2
-              ? SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: ListView.separated(
-                    itemBuilder: (context, index) {
-                      return const HomeworkShimmer();
-                    },
-                    separatorBuilder: (context, index) =>
-                        const VerticalSizedBox(10),
-                    itemCount: 10,
-                  ),
-                )
-              : RefreshIndicator(
-                  onRefresh: () async {
-                    //homeworksController.getHomeworks();
-                  },
-                  child: 'true' == true
-                      ? SizedBox(
-                          height: MediaQuery.of(context).size.height,
-                          child: Center(
-                            child: ListView(
-                              shrinkWrap: true,
-                              children: [
-                                Text(
-                                  "لايوجد وظائف!",
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .titleLarge!
-                                      .copyWith(
-                                        fontSize: 16,
-                                        color: AppColors.lightBlack,
-                                      ),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ],
-                            ),
-                          ),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: BlocBuilder<HomeworksCubit, HomeworksState>(
+              bloc: BlocProvider.of<HomeworksCubit>(context),
+              builder: (context, state) {
+                if (state is HomeworksInitial) {
+                  context.read<HomeworksCubit>().getHomeworks();
+                }
+                if (state is HomeworksLoading) {
+                  return SizedBox(
+                    height: MediaQuery.of(context).size.height,
+                    child: ListView.separated(
+                      itemBuilder: (context, index) {
+                        return const HomeworkShimmer();
+                      },
+                      separatorBuilder: (context, index) =>
+                          const VerticalSizedBox(10),
+                      itemCount: 10,
+                    ),
+                  );
+                }
+                if (state is HomeworksLoadingSuccess) {
+                  final homeworks = state.homeworks;
+                  return homeworks.isEmpty
+                      ? Text(
+                          "لايوجد وظائف!",
+                          style:
+                              Theme.of(context).textTheme.titleLarge!.copyWith(
+                                    fontSize: 16,
+                                    color: AppColors.lightBlack,
+                                  ),
+                          textAlign: TextAlign.center,
                         )
                       : SizedBox(
                           child: ListView.separated(
                             itemBuilder: (context, index) {
-                              return const HomeworkItem();
+                              return HomeworkItem(
+                                homework: homeworks[index],
+                              );
                             },
                             separatorBuilder: (context, index) =>
                                 const VerticalSizedBox(10),
-                            itemCount: 10,
+                            itemCount: homeworks.length,
                           ),
-                        ),
-                ),
+                        );
+                }
+                if (state is HomeworksLoadingError) {
+                  final errorMessage = state.errormsg;
+                  return Text('Error: $errorMessage');
+                }
+                return const SizedBox.shrink();
+              },
+            ),
+          ),
         ),
         bottomNavigationBar: const AppBottomNavigationBar(),
       ),
