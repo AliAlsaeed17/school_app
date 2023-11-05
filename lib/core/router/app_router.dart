d61e94520a329101d1d72f45517bef397c0ee673
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-05 04:57:56 -0800

    Add new screens to router

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,42 +1,63 @@
 import 'package:school_app/core/constants/app_packages.dart';
-import 'package:school_app/logic/cubits/subjects/subjects_cubit.dart';
-import 'package:school_app/view/home/screens/home_screen.dart';
-import 'package:school_app/view/homeworks/screens/homeworks_screen.dart';
-import 'package:school_app/view/subjects/screens/subjects_screen.dart';
-import 'package:school_app/view/vacations/screens/vacations_screen.dart';
+import 'package:school_app/view/installments/screens/installments_screen.dart';
 
 class AppRouter {
   static Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case AppRoutes.splashScreen:
         return MaterialPageRoute(
-          builder: (_) => SplashScreen(),
+          builder: (_) => const SplashScreen(),
         );
       case AppRoutes.loginScreen:
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
             create: (context) => AuthCubit(),
-            child: LoginScreen(),
+            child: const LoginScreen(),
           ),
         );
       case AppRoutes.homeScreen:
         return MaterialPageRoute(
-          builder: (_) => HomeScreen(),
+          builder: (_) => const HomeScreen(),
         );
       case AppRoutes.subjectsScreen:
         return MaterialPageRoute(
           builder: (_) => BlocProvider(
             create: (context) => SubjectsCubit(),
-            child: SubjectsScreen(),
+            child: const SubjectsScreen(),
           ),
         );
       case AppRoutes.homeworksScreen:
         return MaterialPageRoute(
-          builder: (_) => HomeworksScreen(),
+          builder: (_) => BlocProvider(
+            create: (context) => HomeworksCubit(),
+            child: const HomeworksScreen(),
+          ),
         );
       case AppRoutes.vacationsScreen:
         return MaterialPageRoute(
-          builder: (_) => VacationsScreen(),
+          builder: (_) => const VacationsScreen(),
+        );
+      case AppRoutes.resultsScreen:
+        return MaterialPageRoute(
+          builder: (_) => const ExamResultsScreen(),
+        );
+      case AppRoutes.installmentsScreen:
+        return MaterialPageRoute(
+          builder: (_) => const InstallmentsScreen(),
+        );
+      case AppRoutes.complaintsScreen:
+        return MaterialPageRoute(
+          builder: (_) => const ComplaintsScreen(),
+        );
+      case AppRoutes.complaintScreen:
+        return MaterialPageRoute(
+          builder: (_) => ComplaintScreen(
+            complaint: settings.arguments as Complaint,
+          ),
+        );
+      case AppRoutes.teacherNotesScreen:
+        return MaterialPageRoute(
+          builder: (_) => const TeacherNotesScreen(),
         );
     }
     return null;
