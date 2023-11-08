e3eba6f4e3a29a5277268adb472bd66b4576468a
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-08 04:12:16 -0800

    Add new routes

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,8 +1,12 @@
 class AppRoutes {
   static const splashScreen = '/';
   static const loginScreen = '/login';
+  static const registerScreen = '/register';
   static const homeScreen = '/home';
   static const notificationsScreen = '/notifications';
+  static const programScreen = '/program';
+  static const classesScreen = '/classes';
+  static const profileScreen = '/profile';
   static const subjectsScreen = '/subjects';
   static const homeworksScreen = '/homeworks';
   static const vacationsScreen = '/vacations';
