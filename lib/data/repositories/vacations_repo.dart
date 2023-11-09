3080fc4f085837b6e94bc915c37c97fc9b1b26f3
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-09 02:52:22 -0800

    Update return data type of get vacations

--- lib/data/repositories/vacations_repo.dart
+++ lib/data/repositories/vacations_repo.dart
@@ -3,7 +3,7 @@ import 'package:school_app/core/constants/app_packages.dart';
 class VacationsRepo {
   VacationsClient client = VacationsClient();
 
-  Future<List<Homework>> getVacations() async {
+  Future<List<Vacation>> getVacations() async {
     var response = await client.getVacations();
     if (response != "") {
       return response.map<Vacation>((json) => Vacation.fromJson(json)).toList();
