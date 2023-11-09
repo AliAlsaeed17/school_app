9fb6695bdd737b1cede2faaccbbcae785b47f5f1
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-09 02:55:20 -0800

    Make grid view adaptive

--- lib/view/home/widgets/home_navigation_grid_view.dart
+++ lib/view/home/widgets/home_navigation_grid_view.dart
@@ -8,73 +8,83 @@ class HomeNavigationGridView extends StatelessWidget {
     return Expanded(
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 14),
-        child: GridView(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-            mainAxisExtent: 120,
+        child: LayoutBuilder(
+          builder: (context, constraints) => GridView(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: constraints.maxWidth > 700
+                  ? 4
+                  : constraints.maxWidth > 500
+                      ? 3
+                      : constraints.maxWidth > 300
+                          ? 2
+                          : 1,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20,
+              mainAxisExtent: 120,
+              childAspectRatio: constraints.maxWidth / constraints.maxHeight,
+            ),
+            children: [
+              HomeNavigationItem(
+                title: 'المواد',
+                icon: Icons.auto_stories,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.subjectsScreen),
+              ),
+              HomeNavigationItem(
+                title: 'الوظائف',
+                icon: Icons.chrome_reader_mode_outlined,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.homeworksScreen),
+              ),
+              HomeNavigationItem(
+                title: 'العطل',
+                icon: Icons.calendar_month_sharp,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.vacationsScreen),
+              ),
+              HomeNavigationItem(
+                title: 'النتائج',
+                icon: Icons.task_alt,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.resultsScreen),
+              ),
+              HomeNavigationItem(
+                title: 'دوام الطالب',
+                icon: Icons.checklist_rtl,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.studentTimeScreen),
+              ),
+              HomeNavigationItem(
+                title: 'الباص',
+                icon: Icons.bus_alert,
+                onTap: () => Navigator.pushNamed(context, AppRoutes.busScreen),
+              ),
+              HomeNavigationItem(
+                title: 'التنبيهات',
+                icon: Ionicons.alert_circle_sharp,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.alertsScreen),
+              ),
+              HomeNavigationItem(
+                title: 'الأقساط',
+                icon: FontAwesomeIcons.moneyBill1,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.installmentsScreen),
+              ),
+              HomeNavigationItem(
+                title: 'الشكاوى',
+                icon: Icons.feedback,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.complaintsScreen),
+              ),
+              HomeNavigationItem(
+                title: 'ملاحظات المعلم',
+                icon: Icons.note,
+                onTap: () =>
+                    Navigator.pushNamed(context, AppRoutes.teacherNotesScreen),
+              ),
+            ],
           ),
-          children: [
-            HomeNavigationItem(
-              title: 'المواد',
-              icon: Icons.auto_stories,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.subjectsScreen),
-            ),
-            HomeNavigationItem(
-              title: 'الوظائف',
-              icon: Icons.chrome_reader_mode_outlined,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.homeworksScreen),
-            ),
-            HomeNavigationItem(
-              title: 'العطل',
-              icon: Icons.calendar_month_sharp,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.vacationsScreen),
-            ),
-            HomeNavigationItem(
-              title: 'النتائج',
-              icon: Icons.task_alt,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.resultsScreen),
-            ),
-            HomeNavigationItem(
-              title: 'دوام الطالب',
-              icon: Icons.checklist_rtl,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.studentTimeScreen),
-            ),
-            HomeNavigationItem(
-              title: 'الباص',
-              icon: Icons.bus_alert,
-              onTap: () => Navigator.pushNamed(context, AppRoutes.busScreen),
-            ),
-            HomeNavigationItem(
-              title: 'التنبيهات',
-              icon: Ionicons.alert_circle_sharp,
-              onTap: () => Navigator.pushNamed(context, AppRoutes.alertsScreen),
-            ),
-            HomeNavigationItem(
-              title: 'الأقساط',
-              icon: FontAwesomeIcons.moneyBill1,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.installmentsScreen),
-            ),
-            HomeNavigationItem(
-              title: 'الشكاوى',
-              icon: Icons.feedback,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.complaintsScreen),
-            ),
-            HomeNavigationItem(
-              title: 'ملاحظات المعلم',
-              icon: Icons.note,
-              onTap: () =>
-                  Navigator.pushNamed(context, AppRoutes.teacherNotesScreen),
-            ),
-          ],
         ),
       ),
     );
