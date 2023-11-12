f46aa9319b3f7648930feb69dd4461d458bc0a34
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-12 04:44:56 -0800

    Refactor screen code to display loaded homework

--- lib/view/homeworks/screens/homeworks_screen.dart
+++ lib/view/homeworks/screens/homeworks_screen.dart
@@ -1,5 +1,4 @@
 import 'package:school_app/core/constants/app_packages.dart';
-import 'package:school_app/logic/cubits/homeworks/homeworks_cubit.dart';
 
 class HomeworksScreen extends StatefulWidget {
   const HomeworksScreen({super.key});
@@ -10,64 +9,40 @@ class HomeworksScreen extends StatefulWidget {
 
 class _HomeworksScreenState extends State<HomeworksScreen> {
   @override
+  void initState() {
+    BlocProvider.of<HomeworksCubit>(context).getHomeworks();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: mainAppBar(),
       drawer: const AppDrawer(),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(15),
-        child: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: BlocBuilder<HomeworksCubit, HomeworksState>(
-            bloc: BlocProvider.of<HomeworksCubit>(context),
-            builder: (context, state) {
-              if (state is HomeworksInitial) {
-                context.read<HomeworksCubit>().getHomeworks();
-              }
-              if (state is HomeworksLoading) {
-                return SizedBox(
-                  height: MediaQuery.of(context).size.height,
-                  child: ListView.separated(
-                    itemBuilder: (context, index) {
-                      return const HomeworkShimmer();
-                    },
-                    separatorBuilder: (context, index) =>
-                        const VerticalSizedBox(10),
-                    itemCount: 10,
-                  ),
-                );
-              }
-              if (state is HomeworksLoadingSuccess) {
-                final homeworks = state.homeworks;
-                return homeworks.isEmpty
-                    ? Text(
-                        "لايوجد وظائف!",
-                        style: Theme.of(context).textTheme.titleLarge!.copyWith(
-                              fontSize: 16,
-                              color: AppColors.lightBlack,
-                            ),
-                        textAlign: TextAlign.center,
-                      )
-                    : SizedBox(
-                        child: ListView.separated(
-                          itemBuilder: (context, index) {
-                            return HomeworkItem(
-                              homework: homeworks[index],
-                            );
-                          },
-                          separatorBuilder: (context, index) =>
-                              const VerticalSizedBox(10),
-                          itemCount: homeworks.length,
-                        ),
-                      );
-              }
-              if (state is HomeworksLoadingError) {
-                final errorMessage = state.errormsg;
-                return Text('Error: $errorMessage');
-              }
-              return const SizedBox.shrink();
-            },
-          ),
+        height: ResponsiveHelper.screenHeight(context),
+        child: BlocBuilder<HomeworksCubit, HomeworksState>(
+          bloc: BlocProvider.of<HomeworksCubit>(context),
+          builder: (context, state) {
+            if (state is HomeworksLoading) {
+              return const HomeworksShimmerList();
+            } else if (state is HomeworksLoadingSuccess) {
+              return RefreshIndicator(
+                onRefresh: () async =>
+                    context.read<HomeworksCubit>().getHomeworks(),
+                child: HomeworksList(homeworks: state.homeworks),
+              );
+            } else if (state is HomeworksLoadingError) {
+              return Center(
+                child: Text(
+                  'Error: ${state.errormsg}',
+                  textAlign: TextAlign.center,
+                ),
+              );
+            }
+            return const SizedBox.shrink();
+          },
         ),
       ),
       bottomNavigationBar: const AppBottomNavigationBar(),
