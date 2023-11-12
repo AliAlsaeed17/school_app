5c36a4233d4d2d25fd8e7bfa5b78d603e36d0bff
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-12 04:46:58 -0800

    Add certer widget

--- lib/view/shared/loading_item.dart
+++ lib/view/shared/loading_item.dart
@@ -10,8 +10,10 @@ class LoadingItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      color: color,
+    return Center(
+      child: CircularProgressIndicator(
+        color: color,
+      ),
     );
   }
 }
