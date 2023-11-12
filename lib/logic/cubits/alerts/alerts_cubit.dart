2667dfa233389bae70304ab291ce3404ba6f7263
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-12 04:53:21 -0800

    Create alerts cubit class

---
+++ lib/logic/cubits/alerts/alerts_cubit.dart
@@ -0,0 +1,21 @@
+import 'package:school_app/core/constants/app_packages.dart';
+import 'package:school_app/data/repositories/alerts_repo.dart';
+
+part 'alerts_state.dart';
+
+class AlertsCubit extends Cubit<AlertsState> {
+  AlertsCubit() : super(AlertsInitial());
+
+  AlertsRepo alertsRepo = AlertsRepo();
+
+  Future<void> getAlerts() async {
+    emit(AlertsLoading());
+    try {
+      await Future.delayed(const Duration(milliseconds: 1000));
+      List<Alert> alerts = await alertsRepo.getAlerts();
+      emit(AlertsLoadingSuccess(alerts: alerts));
+    } catch (e) {
+      emit(AlertsLoadingError(errormsg: e.toString()));
+    }
+  }
+}
