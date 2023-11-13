4ed5a9216098aa215d6934cf6fc7e67be87112f5
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-13 04:32:16 -0800

    Update screen to display loaded notifications

--- lib/view/notifications/screens/notifications_screen.dart
+++ lib/view/notifications/screens/notifications_screen.dart
@@ -1,5 +1,4 @@
 import 'package:school_app/core/constants/app_packages.dart';
-import 'package:school_app/view/notifications/widgets/notification_shimmer.dart';
 
 class NotificationsScreen extends StatefulWidget {
   const NotificationsScreen({super.key});
@@ -10,47 +9,32 @@ class NotificationsScreen extends StatefulWidget {
 
 class _NotificationsScreenState extends State<NotificationsScreen> {
   @override
+  void initState() {
+    BlocProvider.of<NotificationsCubit>(context).getNotifications();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: mainAppBar(title: 'الاشعارات'),
       drawer: const AppDrawer(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          children: [
-            Expanded(
-              child: 3 == 4
-                  ? ListView.separated(
-                      itemBuilder: (context, i) {
-                        return const NotificationShimmer();
-                      },
-                      separatorBuilder: (context, index) =>
-                          const VerticalSizedBox(10),
-                      itemCount: 10,
-                    )
-                  : RefreshIndicator(
-                      onRefresh: () async {},
-                      child: 3 == 4
-                          ? Center(
-                              child: Text(
-                                "لايوجد اشعارات!",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge!
-                                    .copyWith(color: AppColors.lightBlack),
-                              ),
-                            )
-                          : ListView.separated(
-                              itemBuilder: (context, index) {
-                                return NotificationItem();
-                              },
-                              separatorBuilder: (context, index) =>
-                                  const VerticalSizedBox(10),
-                              itemCount: 4,
-                            ),
-                    ),
-            ),
-          ],
+      body: Container(
+        padding: AppSizes.padding10,
+        height: ResponsiveHelper.screenHeight(context),
+        child: BlocBuilder<NotificationsCubit, NotificationsState>(
+          bloc: BlocProvider.of<NotificationsCubit>(context),
+          builder: (context, state) {
+            if (state is NotificationsLoading) {
+              return const NotificationsShimmerList();
+            } else if (state is NotificationsLoadingSuccess) {
+              return RefreshIndicator(
+                onRefresh: () async {},
+                child: NotificationsList(notifications: state.notifications),
+              );
+            }
+            return const SizedBox.shrink();
+          },
         ),
       ),
       bottomNavigationBar: const AppBottomNavigationBar(),
