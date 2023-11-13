91358bc67448bca3e67120b278cfecc4dcb3af42
AliAlsaeed176 <alialsaeed176@gmail.com>
2023-11-13 04:20:19 -0800

    Add new api links

--- lib/core/constants/app_api_links.dart
+++ lib/core/constants/app_api_links.dart
@@ -4,16 +4,27 @@ class AppApiLinks {
   static const storageBaseUrl = 'http://school.brain.sy/storage/';
 //-----------------------------Auth-----------------------------------
   static const loginApi = '${baseUrl}login';
+//-------------------------Notifications------------------------------
+  static const notifications = '${baseUrl}notifications';
 //----------------------------Subjects--------------------------------
   static const subjects = "${baseUrl}subjectWithHomework/1/1";
 //----------------------------Homeworks-------------------------------
   static const homeworks = "${baseUrl}studentHomework/1";
 //----------------------------Vacations-------------------------------
   static const vacations = "${baseUrl}vacations";
-//-----------------------------Results-------------------------------
+//-----------------------------Results--------------------------------
   static const exams = "${baseUrl}exams/1";
-//-----------------------------Busses-------------------------------
+//-----------------------------Busses---------------------------------
   static const busses = "${baseUrl}deliveryBusById/2";
-//-----------------------------Alerts-------------------------------
+//---------------------------Student Time-----------------------------
+  static const studentTime = "${baseUrl}studentAbsenceById/1";
+//-----------------------------Alerts---------------------------------
   static const alerts = "${baseUrl}alertByStudentId/28";
+//--------------------------Installments------------------------------
+  static const installments = "${baseUrl}financialTransactionById/1";
+//---------------------------Complaints-------------------------------
+  static const complaints = "${baseUrl}complaintByUserId/1";
+  static const complaint = "${baseUrl}complaint";
+//--------------------------Teacher Notes-----------------------------
+  static const teacherNotes = "${baseUrl}notesById/28";
 }
